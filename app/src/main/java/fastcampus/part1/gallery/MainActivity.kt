5eb9c9a8c8fb767f75a60a391bc2c921142fbc4c
1three <1_three@naver.com>
2024-01-26 14:39:39 +0900

    feat: 권한 요청 (성공)

--- app/src/main/java/fastcampus/part1/gallery/MainActivity.kt
+++ app/src/main/java/fastcampus/part1/gallery/MainActivity.kt
@@ -1,8 +1,13 @@
 package fastcampus.part1.gallery
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import fastcampus.part1.gallery.databinding.ActivityMainBinding
 
 /**
@@ -40,24 +45,54 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.loadImageButton.setOnClickListener {
-            checkPermission()
+            checkReadImagesPermission()
         }
     }
 
-    private fun checkPermission() {
+    private fun checkReadImagesPermission() {
+        // 권한 유무 확인 및 요청
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_MEDIA_IMAGES
+            ) == PackageManager.PERMISSION_GRANTED -> loadImage()
+
+            shouldShowRequestPermissionRationale(
+                Manifest.permission.READ_MEDIA_IMAGES
+            ) -> {
+                showReadImagesPermissionRationale()
+            }
+
+            else -> {
+                requestReadImagesPermission()
+            }
+        }
+    }
+
+    private fun showReadImagesPermissionRationale() {
+        AlertDialog.Builder(this).apply {
+            setMessage("이미지를 가져오기 위해\n갤러리 읽기 권한이 필요합니다.")
+            setNegativeButton("취소", null)
+            setPositiveButton("확인") { _, _ ->
+                requestReadImagesPermission()
+            }
+        }.show()
+    }
+
+    private fun requestReadImagesPermission() {
         ActivityCompat.requestPermissions(
             this,
-            arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
-            REQUEST_READ_EXTERNAL_STORAGE
+            arrayOf(android.Manifest.permission.READ_MEDIA_IMAGES),
+            REQUEST_READ_MEDIA_IMAGES
         )
         // requestCode : 작업의 고유 식별자 ('내가' 권한 요청의 고유 식별자 100으로 설정)
     }
 
     private fun loadImage() {
-
+        Toast.makeText(this, "갤러리로 이동합니다.", Toast.LENGTH_SHORT).show()
     }
 
     companion object {
-        const val REQUEST_READ_EXTERNAL_STORAGE = 100
+        const val REQUEST_READ_MEDIA_IMAGES = 100
     }
 }
