f5a8de01d9aefb1e211587c5e5335894cb0ec32c
1three <1_three@naver.com>
2024-01-26 13:57:48 +0900

    feat: 권한 요청 (실패)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "fastcampus.part1.gallery"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "fastcampus.part1.gallery"
         minSdk = 28
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -33,11 +33,13 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    viewBinding {
+        enable = true
+    }
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
--- app/src/main/java/fastcampus/part1/gallery/MainActivity.kt
+++ app/src/main/java/fastcampus/part1/gallery/MainActivity.kt
@@ -2,6 +2,8 @@ package fastcampus.part1.gallery
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import fastcampus.part1.gallery.databinding.ActivityMainBinding
 
 /**
  * 나만의 갤러리 앱
@@ -22,9 +24,40 @@ import android.os.Bundle
  * registerForActivityResult
  * */
 
+/**
+ * 권한 요청 원칙
+ *
+ * 1. 관련 기능 사용 시 권한 요청
+ * 2. 사용자 차단 금지
+ * */
+
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loadImageButton.setOnClickListener {
+            checkPermission()
+        }
+    }
+
+    private fun checkPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+            REQUEST_READ_EXTERNAL_STORAGE
+        )
+        // requestCode : 작업의 고유 식별자 ('내가' 권한 요청의 고유 식별자 100으로 설정)
+    }
+
+    private fun loadImage() {
+
+    }
+
+    companion object {
+        const val REQUEST_READ_EXTERNAL_STORAGE = 100
     }
 }
